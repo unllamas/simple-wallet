00e18ac7ae14025e5c4245a4452a11b70b786a85
Jonathan <jonathan@paxo.co>
2022-09-05 02:11:13 -0300

    fix(page): added TokenWrapper in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { ChakraProvider } from '@chakra-ui/react';
 
 import theme from '../theme';
@@ -5,13 +6,16 @@ import { AppProps } from 'next/app';
 
 import { BlockchainWrapper } from '../context/Blockchain';
 import { AccountWrapper } from '../context/Account';
+import { TokenWrapper } from '../context/Token';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <BlockchainWrapper>
         <AccountWrapper>
-          <Component {...pageProps} />
+          <TokenWrapper>
+            <Component {...pageProps} />
+          </TokenWrapper>
         </AccountWrapper>
       </BlockchainWrapper>
     </ChakraProvider>
