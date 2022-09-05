66444f0db488f601ea662f9a38380e5d1379b419
Jonathan <jonathan@paxo.co>
2022-09-05 02:12:07 -0300

    fix(page): added show token in Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
 import NextLink from 'next/link';
-import { Image, Flex, Box, useDisclosure, Tag, Stat, StatLabel, StatNumber } from '@chakra-ui/react';
+import { Image, Flex, Box, useDisclosure, Tag, Stat, StatLabel, StatNumber, VStack } from '@chakra-ui/react';
 import { RefreshCw, ArrowDownRight, ArrowUpRight } from 'react-feather';
+
+import { useBlockchain } from '../context/Blockchain';
+import { useAccount } from '../context/Account';
+import { useToken } from '../context/Token';
 
 import { Container } from '../components/Container';
 import Modal from '../components/Modal';
@@ -12,7 +17,8 @@ import Button from '../components/Shared/Button';
 const Dashboard = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // const { provider } = useBlockchain();
+  const { wallet } = useAccount();
+  const { tokenETH, tokenDAI } = useToken();
 
   // General
   const [modalType, setModalType] = useState('');
@@ -29,7 +35,7 @@ const Dashboard = () => {
         <Container w='100%' maxW={'md'} px='20px'>
           {/* Balance */}
           <Stat w='100%'>
-            <StatLabel>Collected Fees</StatLabel>
+            <StatLabel>Your balance</StatLabel>
             <StatNumber fontSize='32px'>$0.00</StatNumber>
           </Stat>
 
@@ -41,7 +47,7 @@ const Dashboard = () => {
               </Button>
             </Flex>
             <Flex flex={1} justifyContent={'center'}>
-              <Button type='circle' color='secondary' onClick={''} label='Swap'>
+              <Button type='circle' color='secondary' label='Swap'>
                 <RefreshCw />
               </Button>
             </Flex>
@@ -58,9 +64,28 @@ const Dashboard = () => {
               <Box>
                 <Image src='./75x75.png' maxW='35px' borderRadius='50%' />
               </Box>
+              <Text>ETH</Text>
+            </Flex>
+            <VStack alignItems='flex-end'>
+              <Text>{tokenETH || '0.0'}</Text>
+              <Text size='sm' mt='0px !important'>
+                ${tokenETH || '0.0'}
+              </Text>
+            </VStack>
+          </Flex>
+          <Flex mt='2px' alignItems={'center'} justifyContent={'space-between'} w='100%' bg='#eee' p='20px'>
+            <Flex alignItems={'center'} gap='10px'>
+              <Box>
+                <Image src='./75x75.png' maxW='35px' borderRadius='50%' />
+              </Box>
               <Text>DAI</Text>
             </Flex>
-            <Text>$500.00</Text>
+            <VStack alignItems='flex-end'>
+              <Text>{tokenDAI || '0.0'}</Text>
+              <Text size='sm' mt='0px !important'>
+                ${tokenDAI || '0.0'}
+              </Text>
+            </VStack>
           </Flex>
 
           {/* Security */}
