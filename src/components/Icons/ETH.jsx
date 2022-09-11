8531cff075d09ae822eb0b563a41ce5d24bf99f8
Jona <llamasjonat@gmail.com>
2022-09-11 04:36:46 -0300

    feat(component): icon DAI and ETH added

---
+++ src/components/Icons/ETH.jsx
@@ -0,0 +1,25 @@
+const iconETH = () => {
+  return (
+    <svg width='36' height='36' viewBox='0 0 36 36' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <circle cx='18' cy='18' r='18' fill='#393939' />
+      <g clipPath='url(#clip0_64_108)'>
+        <path d='M18.1372 8L18.0032 8.45564V21.6771L18.1372 21.8109L24.2745 18.1832L18.1372 8Z' fill='#EEEEEE' />
+        <path d='M18.1373 8L12 18.1832L18.1373 21.811V15.3937V8Z' fill='white' />
+        <path
+          d='M18.1373 22.9729L18.0618 23.065V27.7748L18.1373 27.9955L24.2782 19.347L18.1373 22.9729Z'
+          fill='#EEEEEE'
+        />
+        <path d='M18.1373 27.9955V22.9729L12 19.347L18.1373 27.9955Z' fill='white' />
+        <path d='M18.1372 21.8109L24.2744 18.1832L18.1372 15.3937V21.8109Z' fill='#CCCCCC' />
+        <path d='M12 18.1832L18.1372 21.8109V15.3937L12 18.1832Z' fill='#EEEEEE' />
+      </g>
+      <defs>
+        <clipPath id='clip0_64_108'>
+          <rect width='12.2782' height='20' fill='white' transform='translate(12 8)' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default iconETH;
