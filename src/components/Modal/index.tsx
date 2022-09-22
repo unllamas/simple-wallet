722817d31d730404655f8d213baa39707277df91
Jona <llamasjonat@gmail.com>
2022-09-21 23:27:04 -0300

    fix(component): added modal type for Backup in Modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,6 +2,7 @@ import { Modal as ModalBox, ModalOverlay } from '@chakra-ui/react';
 
 import Receive from './Receive';
 import Send from './Send';
+import Backup from './Backup';
 
 interface Modal {
   type: string;
@@ -19,6 +20,7 @@ const Modal = (props: Modal) => {
       <ModalOverlay />
       {type === 'receive' && <Receive />}
       {type === 'send' && <Send onClose={onClose} />}
+      {type === 'backup' && <Backup />}
     </ModalBox>
   );
 };
