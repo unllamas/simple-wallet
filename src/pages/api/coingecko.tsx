21207887044fd0c121193b4ac9f301919f9b9a92
Jona <llamasjonat@gmail.com>
2022-10-08 23:08:08 -0300

    fix(api): added interface for typescript in coingecko

--- src/pages/api/coingecko.tsx
+++ src/pages/api/coingecko.tsx
@@ -1,4 +1,16 @@
-export async function getPrice() {
+interface PriceInterface {
+  success: boolean;
+  data?: {
+    dai: {
+      usd: number;
+    };
+    ethereum: {
+      usd: number;
+    };
+  };
+}
+
+export async function getPrice(): Promise<PriceInterface> {
   const URL = 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cdai&vs_currencies=usd';
   const data = await fetch(URL).then((res) => res.json());
 
