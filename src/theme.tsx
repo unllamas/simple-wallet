e4595396623233f0b1954861c6fc9227cbd755c1
Jona <llamasjonat@gmail.com>
2022-10-30 07:55:31 -0300

    chore: added fontsource in project package

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,12 +1,17 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
+  fonts: {
+    heading: '"Merriweather", serif',
+    body: '"Montserrat", sans-serif',
+  },
   styles: {
     global: {
       'html, body': {
         width: '100%',
         height: '100%',
         overflowX: 'hidden',
+        backgroundColor: '#F8F1E8',
       },
       '#__next': {
         height: '100%',
