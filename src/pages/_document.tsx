cfa73e2250de5600f10f39cfe3a186db82ec7b8b
Jona <llamasjonat@gmail.com>
2022-11-01 15:11:17 -0300

    fix(page): changed description in metadata for _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,20 +14,14 @@ export default class Document extends NextDocument {
           <meta name='robots' content='index,follow' />
 
           <meta property='og:title' content='Sallet - Wallet non custodial' />
-          <meta
-            property='og:description'
-            content='Aceptar cripto-activos nunca fue tan facil. Recibe pagos internacionales en tan solo 30 segundos.'
-          />
+          <meta property='og:description' content='Recibe pagos internacionales en tan solo 30 segundos.' />
           <meta property='og:locale' content='es_ES' />
           <meta property='og:type' content='website' />
           <meta property='og:image' content='/img/social/facebook-1200x630.jpg' />
           <meta property='og:url' content='https://sallet.app' />
 
           <meta name='twitter:title' content='Sallet - Wallet non custodial' />
-          <meta
-            name='twitter:description'
-            content='Aceptar cripto-activos nunca fue tan facil. Recibe pagos internacionales en tan solo 30 segundos.'
-          />
+          <meta name='twitter:description' content='Recibe pagos internacionales en tan solo 30 segundos.' />
           <meta name='twitter:card' content='summary_large_image' />
           <meta name='twitter:image' content='/img/social/twitter-600x330.jpg' />
           <meta name='twitter:url' content='https://sallet.app' />
