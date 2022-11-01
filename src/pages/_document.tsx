edfce6c500f8f1eaab6dfc846e19930f3a7491c0
Jona <llamasjonat@gmail.com>
2022-11-01 15:19:57 -0300

    fix: integrated favicon in _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,17 @@ export default class Document extends NextDocument {
       <Html>
         <Head>
           <title>Sallet - Wallet non custodial</title>
+
+          {/* Favicon */}
+          <link rel='apple-touch-icon' sizes='180x180' href='/img/favicon/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/img/favicon/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/img/favicon/favicon-16x16.png' />
+          <link rel='manifest' href='/img/favicon/site.webmanifest' />
+          <link rel='mask-icon' href='/img/favicon/safari-pinned-tab.svg' color='#5bbad5' />
+          <meta name='msapplication-TileColor' content='#242424' />
+          <meta name='theme-color' content='#F8F1E8'></meta>
+
+          {/* Metadata */}
           <meta
             name='description'
             content='Aceptar cripto-activos nunca fue tan facil. Recibe pagos internacionales en tan solo 30 segundos.'
