2fedfe68f800e324b9c46357166c9f2850a17add
GasparND <gaspardolcemascolo@gmail.com>
2022-11-10 23:10:37 -0300

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+});
+
+const nextConfig = withPWA({
   reactStrictMode: false,
   env: {
     SECRET_KEY: process.env.CRYPTO_SECRET_KEY,
   },
-};
+});
 
 module.exports = nextConfig;
