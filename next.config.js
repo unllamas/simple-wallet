822125b1b664eed0b356f3775f40574fb483f352
GasparND <gaspardolcemascolo@gmail.com>
2022-12-02 21:04:07 -0300

    add: manifest to _document; fix: next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
-const withPWA = require("next-pwa")({
-  dest: "public",
-  runtimeCaching,
-});
+const withPWA = require("next-pwa");
 
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
+  dest: "public",
+  runtimeCaching,
+  skipWaiting: true,
+  register: true,
   env: {
     SECRET_KEY: process.env.CRYPTO_SECRET_KEY,
   },
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import { ColorModeScript } from '@chakra-ui/react';
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
 
 export default class Document extends NextDocument {
   render() {
@@ -9,33 +9,64 @@ export default class Document extends NextDocument {
           <title>Sallet - Wallet non custodial</title>
 
           {/* Favicon */}
-          <link rel='apple-touch-icon' sizes='180x180' href='/img/favicon/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/img/favicon/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/img/favicon/favicon-16x16.png' />
-          <link rel='manifest' href='/img/favicon/site.webmanifest' />
-          <link rel='mask-icon' href='/img/favicon/safari-pinned-tab.svg' color='#111111' />
-          <meta name='msapplication-TileColor' content='#111111' />
-          <meta name='theme-color' content='#111111' />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/img/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/img/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/img/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="manifest" href="/img/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/img/favicon/safari-pinned-tab.svg"
+            color="#111111"
+          />
+          <meta name="msapplication-TileColor" content="#111111" />
+          <meta name="theme-color" content="#111111" />
 
           {/* Metadata */}
           <meta
-            name='description'
-            content='Aceptar cripto-activos nunca fue tan facil. Recibe pagos internacionales en tan solo 30 segundos.'
+            name="description"
+            content="Aceptar cripto-activos nunca fue tan facil. Recibe pagos internacionales en tan solo 30 segundos."
           />
-          <meta name='robots' content='index,follow' />
+          <meta name="robots" content="index,follow" />
 
-          <meta property='og:title' content='Sallet - Wallet non custodial' />
-          <meta property='og:description' content='Recibe pagos internacionales en tan solo 30 segundos.' />
-          <meta property='og:locale' content='es_ES' />
-          <meta property='og:type' content='website' />
-          <meta property='og:image' content='/img/social/facebook-1200x630.jpg' />
-          <meta property='og:url' content='https://sallet.app' />
+          <meta property="og:title" content="Sallet - Wallet non custodial" />
+          <meta
+            property="og:description"
+            content="Recibe pagos internacionales en tan solo 30 segundos."
+          />
+          <meta property="og:locale" content="es_ES" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:image"
+            content="/img/social/facebook-1200x630.jpg"
+          />
+          <meta property="og:url" content="https://sallet.app" />
 
-          <meta name='twitter:title' content='Sallet - Wallet non custodial' />
-          <meta name='twitter:description' content='Recibe pagos internacionales en tan solo 30 segundos.' />
-          <meta name='twitter:card' content='summary_large_image' />
-          <meta name='twitter:image' content='/img/social/twitter-600x330.jpg' />
-          <meta name='twitter:url' content='https://sallet.app' />
+          <meta name="twitter:title" content="Sallet - Wallet non custodial" />
+          <meta
+            name="twitter:description"
+            content="Recibe pagos internacionales en tan solo 30 segundos."
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:image"
+            content="/img/social/twitter-600x330.jpg"
+          />
+          <meta name="twitter:url" content="https://sallet.app" />
         </Head>
         <body>
           <ColorModeScript />
