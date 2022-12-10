7660659dede955303414a83f512cd49d5b7ebabc
Jona <llamasjonat@gmail.com>
2022-12-10 04:54:44 -0300

    fix(hook): value of encrypt converted to string in useCrypto

--- src/hooks/useCrypto.tsx
+++ src/hooks/useCrypto.tsx
@@ -1,15 +1,15 @@
 import CryptoJS from 'crypto-js';
 
-export function encrypt(value: string): string {
-  if(value) {
-    var encode = CryptoJS.AES.encrypt(value, process.env.SECRET_KEY).toString();
-    
+export function encrypt(value): string {
+  if (value) {
+    var encode = CryptoJS.AES.encrypt(JSON.stringify(value), process.env.SECRET_KEY).toString();
+
     return encode;
   }
 }
 
 export function decrypt(value: string): string {
-  if(value){
+  if (value) {
     var bytes = CryptoJS.AES.decrypt(value, process.env.SECRET_KEY);
     var originalText = bytes?.toString(CryptoJS.enc.Utf8);
 
