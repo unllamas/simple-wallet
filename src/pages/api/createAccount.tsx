0db2d20b7333c08fbb24db59e5608620eb6555a0
Jona <llamasjonat@gmail.com>
2023-07-11 20:57:19 -0300

    fix(api): integrated validation for method on createAccount

--- src/pages/api/createAccount.tsx
+++ src/pages/api/createAccount.tsx
@@ -7,39 +7,41 @@ const ecc = require('tiny-secp256k1');
 import { ethers } from 'ethers';
 
 export default async function (req, res) {
-  // Define the network
-  const network = bitcoin?.networks?.bitcoin;
+  if (req.method === 'GET') {
+    // Define the network
+    const network = bitcoin?.networks?.bitcoin;
 
-  // Derivation path
-  const path = `m/49'/1'/0'/0`;
+    // Derivation path
+    const path = `m/49'/1'/0'/0`;
 
-  // Use m/49'/0'/0'/0 for mainnet
-  // Use m/49'/1'/0'/0 for testnet
+    // Use m/49'/0'/0'/0 for mainnet
+    // Use m/49'/1'/0'/0 for testnet
 
-  let mnemonic = bip39?.generateMnemonic();
-  const seed = bip39?.mnemonicToSeedSync(mnemonic);
-  const bip32 = BIP32Factory(ecc);
-  let root = bip32.fromSeed(seed, network);
+    let mnemonic = bip39?.generateMnemonic();
+    const seed = bip39?.mnemonicToSeedSync(mnemonic);
+    const bip32 = BIP32Factory(ecc);
+    let root = bip32.fromSeed(seed, network);
 
-  let account = root?.derivePath(path);
-  let node = account?.derive(0).derive(0);
+    let account = root?.derivePath(path);
+    let node = account?.derive(0).derive(0);
 
-  let walletBTC = bitcoin?.payments?.p2pkh({
-    pubkey: node?.publicKey,
-    network: network,
-  }).address;
+    let walletBTC = bitcoin?.payments?.p2pkh({
+      pubkey: node?.publicKey,
+      network: network,
+    }).address;
 
-  const walletETH = ethers.Wallet.fromMnemonic(mnemonic);
+    const walletETH = ethers.Wallet.fromMnemonic(mnemonic);
 
-  return res.status(200).json({
-    account: {
-      security: {
-        mnemonic,
+    return res.status(200).json({
+      account: {
+        security: {
+          mnemonic,
+        },
+        wallets: {
+          btc: walletBTC,
+          eth: walletETH?.address,
+        },
       },
-      wallets: {
-        btc: walletBTC,
-        eth: walletETH?.address,
-      },
-    },
-  });
+    });
+  }
 }
