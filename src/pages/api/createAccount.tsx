2c4c943100c70c2a6e78064fe153b01c5f7f7b39
Jona <llamasjonat@gmail.com>
2023-07-11 21:17:29 -0300

    fix(api): trycatch for createAccount

--- src/pages/api/createAccount.tsx
+++ src/pages/api/createAccount.tsx
@@ -8,41 +8,45 @@ import { ethers } from 'ethers';
 
 const createAccount = async (req, res) => {
   if (req.method === 'GET') {
-    // Define the network
-    const network = bitcoin?.networks?.bitcoin;
+    try {
+      // Define the network
+      const network = bitcoin?.networks?.bitcoin;
 
-    // Derivation path
-    const path = `m/49'/1'/0'/0`;
+      // Derivation path
+      const path = `m/49'/1'/0'/0`;
 
-    // Use m/49'/0'/0'/0 for mainnet
-    // Use m/49'/1'/0'/0 for testnet
+      // Use m/49'/0'/0'/0 for mainnet
+      // Use m/49'/1'/0'/0 for testnet
 
-    let mnemonic = bip39?.generateMnemonic();
-    const seed = bip39?.mnemonicToSeedSync(mnemonic);
-    const bip32 = BIP32Factory(ecc);
-    let root = bip32.fromSeed(seed, network);
+      let mnemonic = bip39?.generateMnemonic();
+      const seed = bip39?.mnemonicToSeedSync(mnemonic);
+      const bip32 = BIP32Factory(ecc);
+      let root = bip32.fromSeed(seed, network);
 
-    let account = root?.derivePath(path);
-    let node = account?.derive(0).derive(0);
+      let account = root?.derivePath(path);
+      let node = account?.derive(0).derive(0);
 
-    let walletBTC = bitcoin?.payments?.p2pkh({
-      pubkey: node?.publicKey,
-      network: network,
-    }).address;
+      let walletBTC = bitcoin?.payments?.p2pkh({
+        pubkey: node?.publicKey,
+        network: network,
+      }).address;
 
-    const walletETH = ethers.Wallet.fromMnemonic(mnemonic);
+      const walletETH = ethers.Wallet.fromMnemonic(mnemonic);
 
-    return res.status(200).json({
-      account: {
-        security: {
-          mnemonic,
+      return res.status(200).json({
+        account: {
+          security: {
+            mnemonic,
+          },
+          wallet: {
+            btc: walletBTC,
+            eth: walletETH?.address,
+          },
         },
-        wallet: {
-          btc: walletBTC,
-          eth: walletETH?.address,
-        },
-      },
-    });
+      });
+    } catch (error) {
+      return res.status(501).json({ error: error.message });
+    }
   }
 };
 
