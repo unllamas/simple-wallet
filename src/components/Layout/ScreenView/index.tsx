7367e50657180120d58b78c15392557f5eab466b
Jona <llamasjonat@gmail.com>
2023-07-25 04:33:43 -0300

    feat(component): ScreenView added

---
+++ src/components/Layout/ScreenView/index.tsx
@@ -0,0 +1,7 @@
+import { Flex } from '@chakra-ui/react';
+
+const Component = (props) => {
+  return <Flex width={'100%'} height={'100%'} flexDirection={'column'} {...props} />;
+};
+
+export default Component;
