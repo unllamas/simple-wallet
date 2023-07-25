0c07c12256c8da39389a29d1080dd0c04fa905c0
Jona <llamasjonat@gmail.com>
2023-07-24 23:33:18 -0300

    poc(api): coingecko integrated

---
+++ src/pages/api/coingecko.tsx
@@ -0,0 +1,34 @@
+interface PriceInterface {
+  success: boolean;
+  data?: {
+    dai: {
+      usd: number;
+    };
+    ethereum: {
+      usd: number;
+    };
+  };
+}
+
+export async function getPrice() {
+  // const URL = 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cdai&vs_currencies=usd';
+  // const data = await fetch(URL).then((res) => res.json());
+
+  // if (data) {
+  return {
+    success: true,
+    data: {
+      dai: {
+        usd: 1,
+      },
+      ethereum: {
+        usd: 1849.96,
+      },
+    },
+  };
+  // } else {
+  //   return {
+  //     success: false,
+  //   };
+  // }
+}
